df8749280a17fb65f13321863168fe6ba0f515ce
pratikp72 <pratikp72@gmail.com>
2025-03-01 20:17:29 +0530

    fix client issue

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,3 @@
-'use client'
 import Hero from "@/components/hero";
 import ConnectSupabaseSteps from "@/components/tutorial/connect-supabase-steps";
 import SignUpUserSteps from "@/components/tutorial/sign-up-user-steps";
